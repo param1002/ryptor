8bce2b872f3436f890318502e4046d32f8a6be24
param1002 <83985523+param1002@users.noreply.github.com>
2021-05-12 20:55:27 +0530

    initialize project

---
+++ src/main/java/com/param1002/ryptor/RyptorApplication.java
@@ -0,0 +1,13 @@
+package com.param1002.ryptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RyptorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/param1002/ryptor/RyptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.param1002.ryptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
