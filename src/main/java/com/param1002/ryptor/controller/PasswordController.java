90e55fc4c3a6b0579a3b691ee763ff1fa34b2812
param1002 <param_nagar98@hotmail.com>
2021-05-13 22:47:12 +0530

    add cxt path and change endpoint for pwd ctrl

--- src/main/java/com/param1002/ryptor/controller/PasswordController.java
+++ src/main/java/com/param1002/ryptor/controller/PasswordController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/ryptor/v1/passwords")
+@RequestMapping("/passwords")
 public class PasswordController {
 
     @Autowired
