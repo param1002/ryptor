6f0e0eae8cb6cceb000b86f6e6735022bd5f4e52
param1002 <param_nagar98@hotmail.com>
2021-05-13 22:16:50 +0530

    add api version to pwds endpoint

--- src/main/java/com/param1002/ryptor/controller/PasswordController.java
+++ src/main/java/com/param1002/ryptor/controller/PasswordController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/ryptor/passwords")
+@RequestMapping("/ryptor/v1/passwords")
 public class PasswordController {
 
     @Autowired
